025e0c5e28a5d816cac95b7c820c5e4a9d3ca4b6
queenfan93 <queenfan93@gmail.com>
2023-07-24 08:45:51 +0800

    DP

--- Dynamic_Programming/5_Longest_Palindromic_Substring/5_Longest_Palindromic_Substring.c
+++ Dynamic_Programming/5_Longest_Palindromic_Substring/5_Longest_Palindromic_Substring.c
@@ -1,27 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+char * longestPalindrome(char * s){
+    if(strlen(s) == 1) return s;
 
-char* dynamic (char* s, int** dp) {
+    int** dp = malloc(strlen(s) * sizeof(int*));
+    for(int i = 0; i< strlen(s); i++) {
+        dp[i] = calloc(strlen(s), sizeof(int));
+
+    }
+    dp[0][0] = 1;
+    dp[1][1] = 1;
+
     int start = 0, end = 0;
     int max = 0;
 
-    dp[0][0] = 1;
-    dp[1][1] = 1;
-    if(s[0]==s[1]) {
+    if(s[0] == s[1]) {
         dp[0][1] = 2;
         end = 1;
-    }
-    else dp[0][1] = 0;
+    }else 
+        dp[0][1] = 0;
 
     for( int i = 2; i<strlen(s); i++) {
         dp[i][i] = 1;
         for( int j = 0; j < i; j++) {
-            if(s[i]==s[j] && (j+1)==i){
+            if(s[i] == s[j] && (j+1) == i){
                 dp[j][i] = 2;
-            } else if(s[i] == s[j] && dp[j+1][i-1]!= -1){
-                dp[j][i] = dp[j+1][i-1]+2;
+            } else if(s[i] == s[j] && dp[j+1][i-1] != 0){
+                dp[j][i] = dp[j+1][i-1] + 2;
             } 
+
             if(max < dp[j][i]){
                 max = dp[j][i];
                 start = j;
@@ -30,30 +35,11 @@ char* dynamic (char* s, int** dp) {
         }
     }
     
-    char* temp = (char*)malloc(sizeof(char)*(end-start+2));
-    for(int i = start; i<= end; i++) {
+    char* temp = malloc(sizeof(char) * (end-start+2));
+    for(int i = start; i <= end; i++) {
         temp[i-start] = s[i];
     }
 
     temp[end - start + 1] = '\0';
     return temp;
-}
-
-char * longestPalindrome(char * s){
-    if(strlen(s) == 1) return s;
-
-    int** dp = (int**) calloc(strlen(s), sizeof(int*));
-    for(int i = 0; i< strlen(s); i++) {
-        dp[i] = (int*) calloc(strlen(s), sizeof(int));
-        //dp[i] = (int*) memset(dp[i], -1, sizeof(dp[i]));
-    }
-
-    return dynamic(s, dp);
-}
-
-int main() {
-    char s[] = "abacaacb";
-    printf("%s\n", longestPalindrome(s));
-
-    return 0;
 }
